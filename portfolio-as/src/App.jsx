0f133d53abb000590c5ce47f1dd59bb0c5f6920b
Alexander Stöhr <git@alexander-stoehr.de>
2024-08-31 06:57:53 +0200

    added main component imports in app.jsx

--- portfolio-as/src/App.jsx
+++ portfolio-as/src/App.jsx
@@ -1,7 +1,18 @@
+import Projects from "./components/Projects.jsx";
+import Toolbox from "./components/Toolbox.jsx";
+import Hero from "./components/Hero.jsx";
+import About from "./components/About.jsx";
+import Header from "./components/Header.jsx";
+
 function App() {
 
     return (
         <>
+            <Header/>
+            <Hero/>
+            <Projects/>
+            <Toolbox/>
+            <About/>
         </>
     )
 }
---
+++ portfolio-as/src/components/Toolbox.jsx
@@ -0,0 +1,6 @@
+export default function Toolbox() {
+    return (
+        <>
+        </>
+    )
+}
