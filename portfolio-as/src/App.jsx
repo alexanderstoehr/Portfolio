0b95cc3ab5ea334416dcd998d704588cd302b13d
Alexander Stöhr <git@alexander-stoehr.de>
2024-09-14 20:50:05 +0200

    added footer

--- portfolio-as/src/App.jsx
+++ portfolio-as/src/App.jsx
@@ -4,6 +4,7 @@ import Hero from "./components/Hero.jsx";
 import About from "./components/About.jsx";
 import Header from "./components/Header.jsx";
 import {useEffect, useState} from "react";
+import Footer from "./components/Footer.jsx";
 
 function App() {
 
@@ -18,12 +19,14 @@ function App() {
     }
 
     return (
-        <div className="flex flex-col align-middle gap-8 mt-4 w-full max-w-screen-lg mx-auto w-screen px-8 xl:w-[1200px] xl:p-0">
+        <div
+            className="flex min-h-screen flex-col align-middle gap-8 mt-4 w-full max-w-screen-lg mx-auto w-screen px-8 xl:w-[1200px] xl:p-0">
             <Header theme={theme} toggleTheme={toggleTheme}/>
             <Hero/>
             <Projects/>
             <Toolbox/>
             <About/>
+            <Footer theme={theme}/>
         </div>
     )
 }
--- portfolio-as/src/components/Projects.jsx
+++ portfolio-as/src/components/Projects.jsx
@@ -2,9 +2,7 @@ import projects from '/data/projects.json'
 import ProjectListingSingle from "./ProjectListingSingle.jsx";
 
 export default function Projects() {
-
-    console.log(projects)
-
+    
     return (
         <div className="flex-col">
             <div className="">Projects</div>
