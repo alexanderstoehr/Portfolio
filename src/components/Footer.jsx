218ae0bdf2b44373f01d95b38e3ced1db88d50ae
Alexander Stöhr <git@alexander-stoehr.de>
2024-09-18 16:57:33 +0200

    added netlify redirects

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,7 @@
 export default function Footer({theme}) {
     return (
         <div className="flex justify-center mt-auto pb-4">
-            <a href="https://www.amazon.de/hz/wishlist/ls/33N6ZZUM7XDUG?tag=bestwatcher-21" target="_blank">
-                <img src={theme === "dark" ? "/cake-lightmode.svg" : "/cake.svg"} className="hover:animate-pulse"/>
-            </a>
+
         </div>
     )
 }
