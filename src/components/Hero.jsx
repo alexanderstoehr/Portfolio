54791ab2fa30b3dfac958069fe068b9be875c4aa
Alexander Stöhr <git@alexander-stoehr.de>
2024-09-19 22:05:14 +0200

    added json content vars in hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,14 @@
+import data from '/data/content.json'
+
+const {hero} = data.find(item => item.hero);
+
 export default function Hero() {
     return (
         <div className="flex justify-between items-center gap-4">
             <div className="w-1/2 ml-4 ">
-                <div className="text-6xl  font-black">Sup.</div>
-                <div className="text-lg font-bold mb-4">Building Stuff Fullstacky.</div>
-                <div>Hi, I’m Alexander, a web developer from Germany who loves building awesome products with code.</div>
+                <div className="text-6xl  font-black">{hero.heading}</div>
+                <div className="text-lg font-bold mb-4">{hero.subhead}</div>
+                <div>{hero.intro}</div>
             </div>
             <div><img className="blur-sm hover:blur-none" src="/alexander-fullstack-clear.png"/></div>
         </div>
