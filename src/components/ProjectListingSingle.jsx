663118058e542c09fd4a33ed89bda64c81e364cb
Alexander Stöhr <git@alexander-stoehr.de>
2024-09-20 11:51:06 +0200

    added responsiveness to project listings

--- src/components/ProjectListingSingle.jsx
+++ src/components/ProjectListingSingle.jsx
@@ -1,8 +1,8 @@
 export default function ProjectListingSingle({project}) {
     return (
-        <div className="flex flex-col">
+        <div className="flex flex-col md:h-full">
             <div className=""><img className="w-full object-cover" src={project.images[0]}/></div>
-            <div className="p-4 border solid border-gray-200 rounded-b-lg">
+            <div className="p-4 border solid border-gray-200 rounded-b-lg md:flex-grow">
                 <div className="font-bold text-xl mb-2">{project.title}</div>
                 <div className="">{project.shortDescription}</div>
             </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,7 +11,7 @@ export default function Projects() {
         <div className="flex-col">
             <div className="text-4xl font-bold">{projects.heading}</div>
             <div className="font-bold mb-4">{projects.subhead}</div>
-            <div className="grid grid-cols-3 gap-12 auto-rows-fr ">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 auto-rows-fr mt-auto md:h-full ">
                 {myprojects.map((project) => (
                         <ProjectListingSingle key={project.id} project={project}/>
                     )
