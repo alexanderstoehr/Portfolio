de311faabc37ed629368d862b9dfa5ae6316a9c2
Alexander Stöhr <git@alexander-stoehr.de>
2024-09-22 18:10:05 +0200

    adding basic glassmorph

--- src/components/ProjectPopup.jsx
+++ src/components/ProjectPopup.jsx
@@ -10,10 +10,14 @@ export default function ProjectPopup({projectId}) {
     console.log("current project", currentProject)
 
     return (
-        <div className="fixed p12 isolate aspect-video w-96 rounded-xl bg-white/20 shadow-lg ring-1 ring-black inset-0 m-auto">
-            <p>ProjectPopup for ProjectID: {projectId}</p>
-            <p>Project: {currentProject.title}</p>
-
+        <div className="flex justify-center items-center">
+            <div className="fixed inset-0 bg-black/80 z-40"></div>
+            <div className="fixed p-12 rounded-lg border border-white/10 bg-white/10 shadow-lg m-auto backdrop-blur-sm z-50">
+                <div className="relative z-10">
+                    <p>ProjectPopup for ProjectID: {projectId}</p>
+                    <p>Project: {currentProject.title}</p>
+                </div>
+            </div>
         </div>
     )
 }
