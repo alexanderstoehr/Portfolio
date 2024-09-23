60d6ebd50d99e88e95f88a52cadbc3a0940cb76c
Alexander Stöhr <git@alexander-stoehr.de>
2024-09-23 21:32:05 +0200

    added popup columns for project content data

--- src/components/ProjectListingSingle.jsx
+++ src/components/ProjectListingSingle.jsx
@@ -3,7 +3,7 @@ export default function ProjectListingSingle({project, handleProjectClick}) {
 
     return (
         <>
-            <div id={project.id} className="flex flex-col md:h-full cursor-pointer transition duration-300 hover:scale-105"
+            <div id={project.id} className="flex flex-col md:h-full cursor-pointer transition duration-300 hover:scale-105 z-0"
                  onClick={() => handleProjectClick(project.id)}>
                 <div className=""><img className="w-full object-cover" src={project.images[0]}/></div>
                 <div className="p-4 border solid border-gray-200 rounded-b-lg md:flex-grow">
--- src/components/ProjectPopup.jsx
+++ src/components/ProjectPopup.jsx
@@ -10,18 +10,42 @@ export default function ProjectPopup({projectId, handleProjectClick}) {
     console.log("current project", currentProject)
 
     return (
-        <div className="flex justify-center items-center">
+        <div className="fixed inset-0 flex justify-center items-center z-40 m-4">
             <div onClick={() => handleProjectClick(0)} className="fixed inset-0 bg-black/80 z-40"></div>
             <div
-                className="fixed pl-16 pr-24 pt-12 pb-24 rounded-lg border border-white/10 bg-white/10 shadow-lg m-auto backdrop-blur-md z-50">
+                className="absolute max-h-full overflow-y: auto p-6 md:p-24 rounded-lg border border-white/10 bg-white/10 shadow-lg m-auto backdrop-blur-md z-50">
                 <div className=" z-10 text-neutral-300 flex flex-col ">
-                    <img className="cursor-pointer h-6 absolute top-0 right-0 mt-12 mr-12 hover:scale-110"
+                    <img className="cursor-pointer h-6 absolute top-0 right-0 m-4 md:m-12 hover:scale-110"
                          onClick={() => handleProjectClick(0)}
                          src="/close-popup.svg"/>
-                    <div className="mt-8">ProjectPopup for ProjectID: {projectId}</div>
-                    <div>Project: {currentProject.title}</div>
-                    <div>Project: {currentProject.title}</div>
-                    <div>Project: {currentProject.title}</div>
+                    <div className="flex flex-col md:flex-row gap-8 md:gap-24">
+                        <div className="">
+                            <div className="text-xl mb-2 font-bold">{currentProject.title}</div>
+                            <div className="mb-2">{currentProject.shortDescription}</div>
+                            <div className="mb-4">{currentProject.description}</div>
+                            <div className="text-lg font-bold">Highlights</div>
+                            <div className="mb-4">
+                                {currentProject.features.map((feature, ind) => (
+                                    <div key={ind} className="flex gap-2">
+                                        <div className="pl-2">• {feature}</div>
+                                    </div>
+                                ))}
+                            </div>
+                            <div className="text-lg font-bold">Technology</div>
+                            <div className="mb-4">
+                                {currentProject.stack.map((tech, ind) => (
+                                    <div key={ind} className="flex gap-2">
+                                        <div className="pl-2">• {tech}</div>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                        <div className="">
+                            {currentProject.images.map((image, ind) => (
+                                <img key={ind} className="rounded-lg mb-4" src={image}/>
+                            ))}
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
