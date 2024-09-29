70c0529f6346ba377f44d4329de8fdf69500c010
Alexander Stöhr <git@alexander-stoehr.de>
2024-09-29 22:26:09 +0200

    removed clashing project item top border

--- src/components/ProjectListingSingle.jsx
+++ src/components/ProjectListingSingle.jsx
@@ -6,7 +6,7 @@ export default function ProjectListingSingle({project, handleProjectClick}) {
             <div id={project.id} className="flex flex-col md:h-full cursor-pointer transition duration-300 hover:scale-105 z-0"
                  onClick={() => handleProjectClick(project.id)}>
                 <div className=""><img className="w-full object-cover" src={project.images[0]}/></div>
-                <div className="p-4 border solid  border-gray-400 rounded-b-lg md:flex-grow">
+                <div className="p-4 border solid  border-gray-400 border-t-0 rounded-b-lg md:flex-grow">
                     <div className="font-bold text-xl mb-2">{project.title}</div>
                     <div className="">{project.shortDescription}</div>
                 </div>
