60fe3dcba79776a4878c4bd3a644f5cb81dd90fc
Alexander Stöhr <git@alexander-stoehr.de>
2024-09-30 15:31:45 +0200

    added about section content

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,17 @@
+import data from "../../data/content.json";
+import ProjectListingSingle from "./ProjectListingSingle.jsx";
+
+const {about} = data.find(item => item.about)
+
 export default function About() {
     return (
-        <div>
-            About
+        <div className="flex-col">
+            <div className="text-4xl font-bold mb-2">{about.heading}</div>
+            <div className="mb-4">
+                {about.text.split('\n').map((item, key) => {
+                    return <span key={key}>{item}<br/></span>
+                })}
+            </div>
         </div>
     )
 }
