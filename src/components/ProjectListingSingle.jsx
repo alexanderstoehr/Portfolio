49fb271588016e01964826d62d5a92aaa78413b0
Alexander Stöhr <git@alexander-stoehr.de>
2024-10-25 17:26:01 +0200

    replaced borders with light shadow

--- src/components/ProjectListingSingle.jsx
+++ src/components/ProjectListingSingle.jsx
@@ -3,10 +3,11 @@ export default function ProjectListingSingle({project, handleProjectClick}) {
 
     return (
         <>
-            <div id={project.id} className="flex flex-col md:h-full cursor-pointer transition duration-300 hover:scale-105 z-0"
+            <div id={project.id}
+                 className="flex flex-col md:h-full cursor-pointer rounded-2xl transition duration-300 hover:scale-105 z-0 shadow-md shadow-gray-400 dark:shadow-white/10"
                  onClick={() => handleProjectClick(project.id)}>
                 <div className=""><img className="w-full object-cover" src={project.images[0]}/></div>
-                <div className="p-4 border solid  border-gray-400 border-t-0 rounded-b-lg md:flex-grow">
+                <div className="p-4 rounded-b-lg md:flex-grow">
                     <div className="font-bold text-xl mb-2">{project.title}</div>
                     <div className="">{project.shortDescription}</div>
                 </div>
--- src/components/ToolSingle.jsx
+++ src/components/ToolSingle.jsx
@@ -1,7 +1,7 @@
 export default function ToolSingle({tool}) {
     return (
         <div>
-            <div className="mr-4 mb-4 p-2 border border-2 border-gray-400 rounded-lg hover:scale-110">
+            <div className="mr-4 mb-4 p-2  shadow-md shadow-gray-400 dark:shadow-white/10 rounded-lg hover:scale-110">
                 {tool.name}
             </div>
         </div>
