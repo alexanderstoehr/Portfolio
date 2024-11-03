566e27dbb48b0f90b40f7361ab69c2251681567c
Alexander Stöhr <git@alexander-stoehr.de>
2024-11-03 12:23:49 +0100

    added colored shadow on hover

--- src/components/ProjectListingSingle.jsx
+++ src/components/ProjectListingSingle.jsx
@@ -4,7 +4,7 @@ export default function ProjectListingSingle({project, handleProjectClick}) {
     return (
         <>
             <div id={project.id}
-                 className="flex flex-col md:h-full cursor-pointer rounded-2xl transition duration-300 hover:scale-105 z-0 shadow-md shadow-gray-400 dark:shadow-white/10"
+                 className="flex flex-col md:h-full cursor-pointer rounded-2xl transition duration-300 hover:scale-105 z-0 shadow-md shadow-gray-400 hover:shadow-cyan-400 dark:hover:shadow-cyan-300 dark:shadow-white/10"
                  onClick={() => handleProjectClick(project.id)}>
                 <div className="relative">
                     <div className="absolute top-4 right-4 text-sm bg-cyan-500 px-2 py-0.5 rounded ">{project.year}</div>
--- src/components/ToolSingle.jsx
+++ src/components/ToolSingle.jsx
@@ -1,7 +1,8 @@
 export default function ToolSingle({tool}) {
     return (
         <div>
-            <div className="mr-4 mb-4 p-2  shadow-md shadow-gray-400 dark:shadow-white/10 rounded-lg hover:scale-110">
+            <div
+                className="mr-4 mb-4 p-2  shadow-md shadow-gray-400 dark:shadow-white/10 hover:shadow-cyan-400 dark:hover:shadow-cyan-300 rounded-lg hover:scale-110">
                 {tool.name}
             </div>
         </div>
