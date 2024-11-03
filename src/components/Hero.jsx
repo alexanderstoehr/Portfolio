2222a05e70421f13f3cb58e025fa694f3f4b0e01
Alexander Stöhr <git@alexander-stoehr.de>
2024-11-03 12:48:40 +0100

    hero section edit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,6 +6,7 @@ export default function Hero() {
     return (
         <div className="flex justify-between items-center gap-4 flex-col-reverse md:flex-row">
             <div className="md:w-1/2 md:ml-4 ">
+                <div className="text-md  text-cyan-600 mb-4">{hero.preheading}</div>
                 <div className="text-6xl  font-black">{hero.heading}</div>
                 <div className="text-lg font-bold mb-4">{hero.subhead}</div>
                 <div>{hero.intro}</div>
